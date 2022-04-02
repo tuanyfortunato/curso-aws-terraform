400b87c4494227c4c64469633ed2601fe693095a
Tuany Fortunato do Carmo <tuany.fortunato1990@gmail.com>
2022-04-02 17:14:35 -0300

    Criando um bucket com Terraform

---
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my-test-bucket-98840098801" {
+  bucket = "my-tf-test-bucket-98840098801"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev",
+    Managedby = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.my-test-bucket-98840098801.id
+  acl    = "private"
+}
